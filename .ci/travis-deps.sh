d4dcaa4cb38f0d2ae4fba53266699e73aeca8da3
Silvio Traversaro <pegua1@gmail.com>
2017-02-20 08:29:41 +0100

    Add Travis support

---
+++ .ci/travis-deps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+#if OS is linux or is not set
+if [ "$TRAVIS_OS_NAME" = linux -o -z "$TRAVIS_OS_NAME" ]; then
+    sudo sh -c 'echo "deb http://www.icub.org/ubuntu trusty contrib/science" > /etc/apt/sources.list.d/icub.list'
+    sudo apt-get update
+    sudo apt-get -y --force-yes install -qq libeigen3-dev yarp 
+elif [ "$TRAVIS_OS_NAME" = osx ]; then
+    gem install xcpretty
+    brew update &> /dev/null
+    brew tap robotology/cask
+    brew tap homebrew/x11
+    brew tap homebrew/science
+    brew install eigen yarp
+fi
