e45d07573f8732aa7afe1441c0462a89dcc4edb9
Diego Ferigo <diego.ferigo@iit.it>
2018-06-07 13:01:37 +0200

    Empty implementation of FTShoes

--- FTShoes/src/FTShoes.cpp
+++ FTShoes/src/FTShoes.cpp
@@ -5,3 +5,18 @@
  * This software may be modified and distributed under the terms of the
  * GNU Lesser General Public License v2.1 or any later version.
  */
+
+#include "FTShoes.h"
+
+using namespace wear::suit;
+
+class FTShoes::Impl
+{};
+
+FTShoes::FTShoes()
+    : pImpl{new Impl()}
+{}
+
+// Without this destructor here, the linker complains for
+// undefined reference to vtable
+FTShoes::~FTShoes() = default;
