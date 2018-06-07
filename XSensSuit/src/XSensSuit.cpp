2e70ede291de1d48e997484b307235b3810fbdf0
Diego Ferigo <diego.ferigo@iit.it>
2018-06-07 12:58:36 +0200

    Empty implementation of XSensSuit

--- XSensSuit/src/XSensSuit.cpp
+++ XSensSuit/src/XSensSuit.cpp
@@ -5,3 +5,18 @@
  * This software may be modified and distributed under the terms of the
  * GNU Lesser General Public License v2.1 or any later version.
  */
+
+#include "XSensSuit.h"
+
+using namespace wear::suit;
+
+class XSensSuit::Impl
+{};
+
+XSensSuit::XSensSuit()
+    : pImpl{new Impl()}
+{}
+
+// Without this destructor here, the linker complains for
+// undefined reference to vtable
+XSensSuit::~XSensSuit() = default;
