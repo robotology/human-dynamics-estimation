4f917d882b04b012187784977bdb9594e56b13fc
Diego Ferigo <diego.ferigo@iit.it>
2018-08-02 14:51:21 +0200

    Updated Travis CI to stage-based pipeline
    Updated also the pulled docker image

--- .ci/build_deps.sh
+++ .ci/build_deps.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
+set -e
+set -u
 
-# Build and install idyntree
-git clone https://github.com/robotology/idyntree
-cd idyntree
-mkdir build && cd build
-cmake -G"${TRAVIS_CMAKE_GENERATOR}" \
-      -DCMAKE_BUILD_TYPE=${TRAVIS_BUILD_TYPE} \
-      -DIDYNTREE_USES_KDL=OFF \
-      -DIDYNTREE_USES_YARP=ON \
-      -DIDYNTREE_USES_ICUB_MAIN=OFF \
-      ..
-cmake --build . --config ${TRAVIS_BUILD_TYPE}
-cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
+# Folder where to clone repositories
+GIT_FOLDER=$HOME/git
+mkdir -p $GIT_FOLDER
 
-cd ../..
-rm -r idyntree
+if [ "${TRAVIS_OS_NAME}" = "osx" ] ; then
+     # Build and install ycm
+    cd $GIT_FOLDER
+    git clone --depth 1 -b $DEPS_BRANCH https://github.com/robotology/ycm.git
+    cd ycm
+    mkdir build && cd build
+    cmake .. \
+        -G"$TRAVIS_CMAKE_GENERATOR" \
+        -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX
+    cmake --build . --target install
+
+    # Build and install yarp
+    cd $GIT_FOLDER
+    git clone --depth 1 -b $DEPS_BRANCH https://github.com/robotology/yarp.git
+    cd yarp
+    mkdir build && cd build
+    cmake .. \
+        -G"$TRAVIS_CMAKE_GENERATOR" \
+        -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
+        -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX \
+        -DCREATE_LIB_MATH=ON
+    cmake --build . --config $TRAVIS_BUILD_TYPE --target install
+
+    # Build and install idyntree
+    cd $GIT_FOLDER
+    git clone --depth 1 -b $DEPS_BRANCH https://github.com/robotology/idyntree.git
+    cd idyntree
+    mkdir build && cd build
+    cmake .. \
+        -G"$TRAVIS_CMAKE_GENERATOR" \
+        -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
+        -DCMAKE_INSTALL_PREFIX=$DEPS_INSTALL_PREFIX
+    cmake --build . --config $TRAVIS_BUILD_TYPE --target install
+fi
 
 # Build and install xsense-mvn
+cd $GIT_FOLDER
 git clone https://github.com/robotology-playground/xsens-mvn
 cd xsens-mvn
 mkdir build && cd build
@@ -26,8 +52,4 @@ cmake -G"${TRAVIS_CMAKE_GENERATOR}" \
       -DENABLE_xsens_mvn_wrapper=OFF \
       -DENABLE_xsens_mvn_remote=OFF \
       ..
-cmake --build . --config ${TRAVIS_BUILD_TYPE}
 cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
-
-cd ../..
-rm -r xsens-mvn
--- .ci/install_deps.sh
+++ .ci/install_deps.sh
@@ -1,26 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-case $TRAVIS_OS_NAME in
-    linux)
-        apt-get update
-        apt-get install -y --no-install-recommends \
-            libgsl-dev \
-            coinor-libipopt1v5 \
-            coinor-libipopt-dev \
-            libmatio2 \
-            libmatio-dev \
-            qtbase5-dev \
-            libqt5xmlpatterns5-dev
-        rm -rf /var/lib/apt/lists/*
-    ;;
-    osx)
-        brew install ccache
-        export PATH="/usr/local/opt/ccache/libexec:$PATH"
-        brew update &> /dev/null
-        brew tap robotology/formulae
-        brew install eigen dartsim/dart/ipopt robotology/formulae/yarp qt
-        export PATH="/usr/local/opt/qt/bin/:$PATH"
-    ;;
-    *) exit 1
-    ;;
-esac
+brew tap robotology/formulae
+brew install eigen dartsim/dart/ipopt robotology/formulae/yarp qt
+export PATH="/usr/local/opt/qt/bin/:$PATH"
