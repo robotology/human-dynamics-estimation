4046b941e243908e2115d986ce77dda6c514a4aa
Yeshasvi Tirupachuri <yeshasvitvs@gmail.com>
2018-11-20 18:53:46 +0100

    Update human dynamics interface and CMakeLists for wrappers and publishers

--- interfaces/IHumanDynamics/IHumanDynamics.h
+++ interfaces/IHumanDynamics/IHumanDynamics.h
@@ -9,6 +9,9 @@
 #ifndef HDE_INTERFACES_IHUMANDYNAMICS
 #define HDE_INTERFACES_IHUMANDYNAMICS
 
+#include <string>
+#include <vector>
+
 namespace hde {
     namespace interfaces {
         class IHumanDynamics;
@@ -19,6 +22,11 @@ class hde::interfaces::IHumanDynamics
 {
 public:
     virtual ~IHumanDynamics() = default;
+
+    virtual std::vector<std::string> getJointNames() const = 0;
+    virtual size_t getNumberOfJoints() const = 0;
+
+    virtual std::vector<double> getJointTorques() const = 0;
 };
 
 #endif // HDE_INTERFACES_IHUMANDYNAMICS
