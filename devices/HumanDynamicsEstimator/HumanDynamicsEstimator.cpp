f1687dcb9978683ab2337358f9a0b84aea9a5b00
Yeshasvi Tirupachuri <yeshasvitvs@gmail.com>
2018-12-03 13:33:31 +0100

    Update attach with xsens_human_state_provider

--- devices/HumanDynamicsEstimator/HumanDynamicsEstimator.cpp
+++ devices/HumanDynamicsEstimator/HumanDynamicsEstimator.cpp
@@ -1192,7 +1192,7 @@ bool HumanDynamicsEstimator::attach(yarp::dev::PolyDriver* poly)
     // Get the device name from the driver
     const std::string deviceName = poly->getValue("device").asString();
 
-    if (deviceName == "human_state_provider") {
+    if (deviceName == "human_state_provider" || deviceName == "xsens_human_state_provider") {
 
         // Attach IHumanState interface from HumanStateProvider
         if (pImpl->iHumanState || !poly->view(pImpl->iHumanState) || !pImpl->iHumanState) {
