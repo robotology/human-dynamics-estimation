9bdfe1d892491ef88a81d51e1773de5d8dc05ea9
Kourosh Darvish <kourosh.darvish@gmail.com>
2019-09-30 12:36:54 +0200

    fix the dependencies for the travis: osqp, osqp-eigen, wearables
    fix the bug for travis osqp
    
    fix the travis of osqp-eigen

--- .ci/build_deps.sh
+++ .ci/build_deps.sh
@@ -80,11 +80,38 @@ cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
 # Build and install wearables
 cd $GIT_FOLDER
 rm -rf wearables
-git clone https://github.com/robotology-playground/wearables.git
+git clone https://github.com/robotology/wearables.git
 cd wearables
-git checkout feature/ICub-device-impl
 mkdir -p build && cd build
 cmake -G"${TRAVIS_CMAKE_GENERATOR}" \
       -DCMAKE_BUILD_TYPE=${TRAVIS_BUILD_TYPE} \
       ..
 cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
+
+# Build and install osqp
+cd $GIT_FOLDER
+rm -rf osqp
+git clone --recursive https://github.com/robotology-dependencies/osqp.git
+cd osqp
+git checkout fix-lf-problems
+mkdir -p build && cd build
+cmake -G"${TRAVIS_CMAKE_GENERATOR}" \
+      -DCMAKE_BUILD_TYPE=${TRAVIS_BUILD_TYPE} \
+      -DUNITTESTS:BOOL=OFF \
+      ..
+cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
+
+# Build and install osqp-Eigen
+cd $GIT_FOLDER
+rm -rf OsqpEigen
+git clone https://github.com/robotology/osqp-eigen.git OsqpEigen
+cd OsqpEigen
+git checkout v0.4.1
+mkdir -p build && cd build
+cmake -G"${TRAVIS_CMAKE_GENERATOR}" \
+      -DCMAKE_BUILD_TYPE=${TRAVIS_BUILD_TYPE} \
+      -DBUILD_TESTING:BOOL=OFF\
+      ..
+cmake --build . --config ${TRAVIS_BUILD_TYPE} --target install
+
+
