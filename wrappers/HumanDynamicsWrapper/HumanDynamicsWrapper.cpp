fec0ab1ea8ad21716d35505db9873cdc33217935
yeshasvitvs <yeshasvitvs@gmail.com>
2020-02-25 17:52:26 +0100

    Update thrift headers in wrappers

--- wrappers/HumanDynamicsWrapper/HumanDynamicsWrapper.cpp
+++ wrappers/HumanDynamicsWrapper/HumanDynamicsWrapper.cpp
@@ -8,7 +8,7 @@
 
 #include "HumanDynamicsWrapper.h"
 #include "IHumanDynamics.h"
-#include "thrift/HumanDynamics.h"
+#include <HumanDynamicsEstimation/HumanDynamics.h>
 
 #include <yarp/os/BufferedPort.h>
 #include <yarp/os/LogStream.h>
