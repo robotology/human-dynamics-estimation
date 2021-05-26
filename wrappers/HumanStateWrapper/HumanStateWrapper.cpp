1973fea372221516e3e648b052cd780efedf6c8c
Kourosh Darvish <kourosh.darvish@gmail.com>
2021-05-26 23:17:37 +0200

    fix the thrift msgs installation (#251)
    * fix the thrift msgs installation
    
    * Update wrappers/HumanDynamicsWrapper/HumanDynamicsWrapper.cpp
    
    Co-authored-by: Lorenzo Rapetti <lor.rapetti@gmail.com>
    
    Co-authored-by: Lorenzo Rapetti <lor.rapetti@gmail.com>

--- wrappers/HumanStateWrapper/HumanStateWrapper.cpp
+++ wrappers/HumanStateWrapper/HumanStateWrapper.cpp
@@ -8,7 +8,7 @@
 
 #include "HumanStateWrapper.h"
 #include <hde/interfaces/IHumanState.h>
-#include <HumanDynamicsEstimation/HumanState.h>
+#include <hde/msgs/HumanState.h>
 
 #include <algorithm>
 #include <yarp/os/BufferedPort.h>
