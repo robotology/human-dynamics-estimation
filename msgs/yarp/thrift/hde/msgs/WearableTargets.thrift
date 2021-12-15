aacef3d020fd3d1fff53ee58f36041e931dfb081
Lorenzo <lor.rapetti@gmail.com>
2021-12-15 19:23:40 +0100

    Add Wearable Targets interface and thrift

---
+++ msgs/yarp/thrift/hde/msgs/WearableTargets.thrift
@@ -0,0 +1,40 @@
+namespace yarp hde.msgs
+
+/**
+ * Representation of a 3D vector
+ */
+struct Vector3 {
+    1: double x;
+    2: double y;
+    3: double z;
+}
+
+/**
+ * Representation of a Quaternion
+ */
+struct Quaternion {
+    1: double w;
+    2: Vector3 imaginary;
+}
+
+enum TargetType {
+    POSE,
+    POSEANDVELOCITY,
+    POSITION,
+    POSITIONANDVELOCITY,
+    ORIENTATION,
+    ORIENTATIONANDVELOCITY,
+}
+
+struct WearableTarget {
+    1: string linkName;
+    2: TargetType type;
+    3: Vector3 position;
+    4: Quaternion orientation;
+    5: Vector3 linearVelocity;
+    6: Vector3 angularVelocity;
+}
+
+struct WearableTargets {
+    1: map<string,WearableTarget> targets;
+}
