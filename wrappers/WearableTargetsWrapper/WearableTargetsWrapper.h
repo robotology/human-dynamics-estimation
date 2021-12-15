c7296797fe8c6e9f9bf0b1d41231ae05f90a9524
Lorenzo <lor.rapetti@gmail.com>
2021-12-15 22:23:11 +0100

    Add WearableTargets wrapper device

---
+++ wrappers/WearableTargetsWrapper/WearableTargetsWrapper.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2021 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms of the
+ * GNU Lesser General Public License v2.1 or any later version.
+ */
+
+#ifndef HDE_DEVICES_WEARABLETARGETSWRAPPER
+#define HDE_DEVICES_WEARABLETARGETSWRAPPER
+
+#include <yarp/dev/DeviceDriver.h>
+#include <yarp/dev/Wrapper.h>
+#include <yarp/os/PeriodicThread.h>
+
+#include <memory>
+
+namespace hde {
+    namespace wrappers {
+        class WearableTargetsWrapper;
+    } // namespace wrappers
+} // namespace hde
+
+class hde::wrappers::WearableTargetsWrapper final
+    : public yarp::dev::DeviceDriver
+    , public yarp::dev::IWrapper
+    , public yarp::dev::IMultipleWrapper
+    , public yarp::os::PeriodicThread
+{
+private:
+    class impl;
+    std::unique_ptr<impl> pImpl;
+
+public:
+    WearableTargetsWrapper();
+    ~WearableTargetsWrapper() override;
+
+    // DeviceDriver interface
+    bool open(yarp::os::Searchable& config) override;
+    bool close() override;
+
+    // PeriodicThread
+    void run() override;
+    void threadRelease() override;
+
+    // IWrapper interface
+    bool attach(yarp::dev::PolyDriver* poly) override;
+    bool detach() override;
+
+    // IMultipleWrapper interface
+    bool attachAll(const yarp::dev::PolyDriverList& driverList) override;
+    bool detachAll() override;
+};
+
+#endif // HDE_DEVICES_WEARABLETARGETSWRAPPER
