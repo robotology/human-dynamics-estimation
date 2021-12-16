e099b49f9188cbc9da0ed1e51df74ae3ccfb8092
Lorenzo <lor.rapetti@gmail.com>
2021-12-16 03:15:03 +0100

    Enable targets visualization in HumanStateVisualizer

--- modules/HumanStateVisualizer/src/main.cpp
+++ modules/HumanStateVisualizer/src/main.cpp
@@ -9,6 +9,7 @@
      */
 
 #include <hde/interfaces/IHumanState.h>
+#include <hde/interfaces/IWearableTargets.h>
 
 #include <yarp/os/Network.h>
 #include <yarp/os/LogStream.h>
@@ -23,6 +24,7 @@
 #include <cmath>
 #include <thread>
 #include <csignal>
+#include <unordered_map>
 
 const std::string ModuleName = "HumanStateVisualizer";
 const std::string LogPrefix = ModuleName + " :";
@@ -122,6 +124,14 @@ int main(int argc, char* argv[])
         visualizeFrames = false;
     }
     visualizeFrames = rf.find("visualizeFrames").asBool();
+
+    bool visualizeTargets;
+    if( !(rf.check("visualizeTargets") && rf.find("visualizeTargets").isBool()) ) 
+    {
+        yWarning() << LogPrefix << "'visualizeTargets' option not found or not valid. It is set to False.";
+        visualizeTargets = false;
+    }
+    visualizeTargets = rf.find("visualizeTargets").asBool();
 
     iDynTree::Position cameraDeltaPosition;
     if( !(rf.check("cameraDeltaPosition") && rf.find("cameraDeltaPosition").isList() && rf.find("cameraDeltaPosition").asList()->size() == 3) ) 
@@ -253,18 +263,70 @@ int main(int argc, char* argv[])
         }
     }
 
-    double frameScalingFactor = 1.0;
+    double linksFrameScalingFactor = 1.0;
     if (visualizeFrames)
     {
-        if ( !(rf.check("frameScalingFactor") && rf.find("frameScalingFactor").isDouble()) )
+        if ( !(rf.check("linksFrameScalingFactor") && rf.find("linksFrameScalingFactor").isDouble()) )
         {
-            yWarning() << LogPrefix << "'frameScalingFactor' option not found or not valid. Using default scale factor = 1.0";
+            yWarning() << LogPrefix << "'linksFrameScalingFactor' option not found or not valid. Using default scale factor = 1.0";
         }
         else
         {
-            frameScalingFactor = rf.find("frameScalingFactor").asDouble();
+            linksFrameScalingFactor = rf.find("linksFrameScalingFactor").asDouble();
         }
     }
+
+    // Visualize Targets Options
+    std::vector<hde::TargetName> visualizedTargetsFrame;
+    if (visualizeTargets)
+    {
+        if( !(rf.check("visualizedTargetsFrame") && rf.find("visualizedTargetsFrame").isList()) )
+        {
+            yError() << LogPrefix << "'visualizedTargetsFrame' option not found or not valid. Frames Visualization will be disabled";
+            visualizeTargets = false;
+        }
+        else
+        {
+            auto visualizedTargetsFrameList = rf.find("visualizedTargetsFrame").asList();
+            for (size_t it = 0; it < visualizedTargetsFrameList->size(); it++)
+            {
+                if(!visualizedTargetsFrameList->get(it).isString())
+                {
+                    yError() << LogPrefix << "in 'visualizedTargetsFrame' there is a field that is not a string.";
+                    return EXIT_FAILURE;
+                }
+                visualizedTargetsFrame.push_back(visualizedTargetsFrameList->get(it).asString());
+            }
+        }
+    }
+
+    double targetsFrameScalingFactor = 1.0;
+    if (visualizeTargets)
+    {
+        if ( !(rf.check("targetsFrameScalingFactor") && rf.find("targetsFrameScalingFactor").isDouble()) )
+        {
+            yWarning() << LogPrefix << "'targetsFrameScalingFactor' option not found or not valid. Using default scale factor = 1.0";
+        }
+        else
+        {
+            targetsFrameScalingFactor = rf.find("targetsFrameScalingFactor").asDouble();
+        }
+    }
+
+    std::string wearableTargetsWrapperPortName;
+    if (visualizeTargets)
+    {
+        if( !(rf.check("wearableTargetsWrapperPortName") && rf.find("wearableTargetsWrapperPortName").isString()) )
+        {
+            yError() << LogPrefix << "'wearableTargetsWrapperPortName' option not found or not valid. Wrench Visualization will be disabled";
+            visualizeTargets = false;
+        }
+        else
+        {
+            wearableTargetsWrapperPortName = rf.find("wearableTargetsWrapperPortName").asString();
+        }
+    }
+
 
     // load model
     std::string urdfFilePath = rf.findFile(urdfFile);
@@ -372,6 +434,63 @@ int main(int argc, char* argv[])
         }
     }
 
+    // initialize iWearableTargets interface from remapper
+    std::unordered_map<hde::TargetName, std::shared_ptr<hde::WearableSensorTarget>> targets;
+    yarp::dev::PolyDriver wearableTargetsRemapperDevice;
+    if (visualizeTargets) {
+        hde::interfaces::IWearableTargets* iWearableTargets{nullptr};
+
+        yarp::os::Property wearableTargetsRemapperOptions;
+        wearableTargetsRemapperOptions.put("device", "wearable_targets_remapper");
+        wearableTargetsRemapperOptions.put("wearableTargetsDataPort", wearableTargetsWrapperPortName);
+        std::cerr << "my wrapper port name is: "  << wearableTargetsWrapperPortName << std::endl;
+
+        if(!wearableTargetsRemapperDevice.open(wearableTargetsRemapperOptions))
+        {
+            yError() << LogPrefix << "Failed to connect remapper device";
+            return EXIT_FAILURE;
+        }
+        if(!wearableTargetsRemapperDevice.view(iWearableTargets) || !iWearableTargets )
+        {
+            yError() << LogPrefix << "Failed to view iWearableTargets interface";
+            return EXIT_FAILURE;
+        }
+
+        // wait for the iWearableTargets to be initialized
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        while (iWearableTargets->getAllTargetsName().empty())
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            yInfo() << LogPrefix << "Waiting for data from WearableTargetsRemapper";
+        }
+
+        // Verify the selected targets exist, and that the associated link exists in the visualized model
+        // and create a vector with the targets
+        auto targetsName = iWearableTargets->getAllTargetsName();
+        for (auto visualizedTargetFrame : visualizedTargetsFrame)
+        {
+            auto target = iWearableTargets->getTarget(visualizedTargetFrame);
+
+            // check if target exists
+            if (!target)
+            {
+                yError() << LogPrefix << "target [ " << visualizedTargetFrame << " ] not found in iWearableTargets." ;
+                return EXIT_FAILURE;
+            }
+
+            // check if frame exists
+            auto frameIndex = model.getLinkIndex(target.get()->modelLinkName);
+            if (frameIndex == iDynTree::FRAME_INVALID_INDEX)
+            {
+                yError() << LogPrefix << "target link [ " << target.get()->modelLinkName << " ] not found in the visualized model";
+                return EXIT_FAILURE;
+            }
+
+            // add the target to the vector
+            targets.emplace(visualizedTargetFrame, target);
+        }
+    }
+
     // initialize wrench port
     yarp::os::BufferedPort<yarp::sig::Vector> wrenchPort;
     yarp::sig::Vector* wrenchMeasuresVector;
@@ -449,7 +568,16 @@ int main(int argc, char* argv[])
         for (size_t vectorIndex = 0; vectorIndex < visualizedLinksFrame.size(); vectorIndex++)
         {
             linkTransform = viz.modelViz("human").getWorldLinkTransform(visualizedLinksFrame.at(vectorIndex));
-            viz.frames().addFrame(linkTransform, frameScalingFactor);
+            viz.frames().addFrame(linkTransform, linksFrameScalingFactor);
+        }
+    }
+
+    if (visualizeTargets)
+    {
+        for (auto targetEntity : targets)
+        {
+            linkTransform = iDynTree::Transform(targetEntity.second.get()->getCalibratedRotation(), iDynTree::Position(targetEntity.second.get()->getCalibratedPosition()));
+            viz.frames().addFrame(linkTransform, targetsFrameScalingFactor);
         }
     }
 
@@ -458,6 +586,8 @@ int main(int argc, char* argv[])
     std::chrono::steady_clock::time_point lastViz = std::chrono::steady_clock::now();
 
     long minimumMicroSecViz = std::round(1e6 / (double) maxVisualizationFPS);
+
+    std::cerr << "RUNNING VIZ!" << std::endl;
 
     while(viz.run() && !isClosing)
     {
@@ -515,12 +645,40 @@ int main(int argc, char* argv[])
             }
         }
 
+        size_t framesIterator = 0;
         if (visualizeFrames)
         {
             for (size_t vectorIndex = 0; vectorIndex < visualizedLinksFrame.size(); vectorIndex++)
             {
                 linkTransform = viz.modelViz("human").getWorldLinkTransform(visualizedLinksFrame.at(vectorIndex));
                 viz.frames().updateFrame(vectorIndex, linkTransform);
+                framesIterator++;
+            }
+        }
+
+        if (visualizeTargets)
+        {
+            for (auto targetEntity : targets)
+            {
+                switch (targetEntity.second.get()->targetType)
+                {
+                case hde::KinematicTargetType::pose: case hde::KinematicTargetType::poseAndVelocity: 
+                    linkTransform = iDynTree::Transform(targetEntity.second.get()->getCalibratedRotation(), iDynTree::Position(targetEntity.second.get()->getCalibratedPosition()));
+                    break;
+                case hde::KinematicTargetType::position: case hde::KinematicTargetType::positionAndVelocity:
+                    linkTransform = viz.modelViz("human").getWorldLinkTransform(targetEntity.second.get()->modelLinkName);
+                    linkTransform.setPosition(iDynTree::Position(targetEntity.second.get()->getCalibratedPosition()));
+                    break;
+                case hde::KinematicTargetType::orientation: case hde::KinematicTargetType::orientationAndVelocity:
+                    linkTransform = viz.modelViz("human").getWorldLinkTransform(targetEntity.second.get()->modelLinkName);
+                    linkTransform.setRotation(targetEntity.second.get()->getCalibratedRotation());
+                    break;
+                default:
+                    linkTransform = viz.modelViz("human").getWorldLinkTransform(targetEntity.second.get()->modelLinkName);
+                }
+
+                viz.frames().updateFrame(framesIterator, linkTransform);
+                framesIterator++;
             }
         }
 
@@ -541,6 +699,7 @@ int main(int argc, char* argv[])
 
     viz.close();
     remapperDevice.close();
+    wearableTargetsRemapperDevice.close();
     wrenchPort.close();
 
     return 0;
