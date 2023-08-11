b628acb747d7b625e6a35a44a8137caf95dc52b3
Silvio Traversaro <silvio@traversaro.it>
2023-08-11 12:55:01 +0200

    License the repo under the BSD-3-Clause license

--- devices/HumanControlBoard/tests/HumanSimulinkController/initHumanController.m
+++ devices/HumanControlBoard/tests/HumanSimulinkController/initHumanController.m
@@ -1,19 +1,6 @@
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% /**
-%  * @author: Yeshasvi Tirupachuri
-%  * Permission is granted to copy, distribute, and/or modify this program
-%  * under the terms of the GNU General Public License, version 2 or any
-%  * later version published by the Free Software Foundation.
-%  *
-%  * A copy of the license can be found at
-%  * http://www.robotcub.org/icub/license/gpl.txt
-%  *
-%  * This program is distributed in the hope that it will be useful, but
-%  * WITHOUT ANY WARRANTY; without even the implied warranty of
-%  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-%  * Public License for more details
-%  */
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% SPDX-FileCopyrightText: Fondazione Istituto Italiano di Tecnologia (IIT)
+%% SPDX-License-Identifier: BSD-3-Clause
+
 clear variables
 clc
 
